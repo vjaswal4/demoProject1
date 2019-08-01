175bb2ccbda4263f3b24b7e9c6c58fd0f42a6d35
unknown <vjaswal4@gmail.com>
2019-07-31 20:17:23 -0400

    First Commit

---
+++ src/test/java/com/Automation/Talks/demoProject2/TestClass1.java
@@ -0,0 +1,59 @@
+package com.Automation.Talks.demoProject2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+	public static WebDriver driver;
+
+	@BeforeMethod
+	public void launchDriver() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\17786\\chromedriver_win32\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.manage().window().fullscreen();
+	}
+
+	@Test
+	public void test1() {
+
+		driver.navigate().to("https://automationtalks.com/");
+
+		System.out.println("Test 1 title is " + driver.getTitle());
+
+	}
+
+	@Test
+	public void test2() {
+
+		driver.navigate().to("https://automationtalks.com/");
+
+		System.out.println("Test 2 title is " + driver.getTitle());
+
+	}
+
+	@Test
+	public void test3() {
+
+		driver.navigate().to("https://automationtalks.com/");
+
+		System.out.println("Testest 3 title is " + driver.getTitle());
+
+	}
+
+	@AfterMethod
+	public void quit() {
+
+		driver.quit();
+	}
+
+}
